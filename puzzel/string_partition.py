dab81615638f3afaed508116e874f06a46788fa0
singhsatnam <satnamsingh8404@gmail.com>
2021-05-13 08:30:42 -0600

    added partition labels

--- puzzel/string_partition.py
+++ puzzel/string_partition.py
@@ -6,12 +6,15 @@ def partition_labels(S: str) -> List[int]:
     last_occur = {}
     for i in range(len(S)):
         last_occur[S[i]] = i
+    print("last occur: ", last_occur)
     last = 0
     start = 0
     for i in range(len(S)):
+        print("i=", i, " ele=", S[i])
         last = max(last, last_occur[S[i]])
-        print(last)
+        print("last=", last)
         if last == i:
+            print(last, "==", i)
             add_this = last - start + 1
             res.append(add_this)
             start = last + 1
