8796197914201a0099e7a684418835d251d11c53
singhsatnam <satnamsingh8404@gmail.com>
2021-05-16 23:07:40 -0600

    modified buy sell stock

--- puzzel/buy_sell_stock.py
+++ puzzel/buy_sell_stock.py
@@ -1,28 +1,18 @@
-from typing import List
+import sys
 
 
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        size = len(prices)
-        l = 0
-        r = size - 1
-        max = 0
-        while (l != r):
-            print("l: ", l, " r: ", r)
-            cur = prices[r] - prices[l]
-            print("curPrice:", cur)
-            print("l-r: ", cur)
-            max = max if max > cur else cur
-            print(max)
-            if (prices[l] > prices[r]):
-                print("more")
-                l += 1
-            else:
-                print("less")
-                r -= 1
-        print("Final max: ", max)
-        return max
+    def maxProfit(self, prices):
+        minprice = float("inf")
+        maxprofit = 0
+        for i in range(len(prices)):
+            if (prices[i] < minprice):
+                minprice = prices[i]
+            elif prices[i] - minprice > maxprofit:
+                maxprofit = prices[i] - minprice
+        return maxprofit
 
 
 solution = Solution()
-solution.maxProfit([7, 1, 5, 3, 6, 4])
+print(solution.maxProfit([7, 1, 5, 3, 6, 4]))
+print(solution.maxProfit([2, 6, 1]))
