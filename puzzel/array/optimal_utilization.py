3f5271313205a4a658f24843c833ff625a7e22f6
singhsatnam <satnamsingh8404@gmail.com>
2021-06-07 22:17:56 -0600

    completed optimal utilization

--- puzzel/array/optimal_utilization.py
+++ puzzel/array/optimal_utilization.py
@@ -10,13 +10,24 @@ class Solution:
         best = 0
         res = []
         for ele_a in sorted_a:
+            print()
+            print("ele a:", ele_a)
             for ele_b in sorted_b:
+                print("ele b:", ele_b)
                 cur_sum = ele_a[1] + ele_b[1]
-                if best < cur_sum <= target:
-                     res.append([ele_a[0], ele_b[0]])
+                print(cur_sum, ele_a[0], ele_b[0])
+                if cur_sum <= target:
+                    if cur_sum > best:
+                        best = cur_sum
+                        res = []
+                        res.append([ele_a[0], ele_b[0]])
+                    elif cur_sum == best:
+                        res.append([ele_a[0], ele_b[0]])
 
         return res
 
 
 soln = Solution()
-print(soln.get_valuesclosest_to_target([[1, 6], [2, 2], [3, 4]], [[1, 2]], 7))
+print(soln.get_valuesclosest_to_target([[1, 3], [2, 5], [3, 7], [4, 10]],
+                                       [[1, 2], [2, 3], [3, 4], [4, 5]],
+                                       10))
